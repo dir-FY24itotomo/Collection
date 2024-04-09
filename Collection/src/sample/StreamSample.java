cb0c85f5f06b35a80fda81d37170af01cce4db17
dir-FY24itotomo <tomo.ito.b503@dirbato.co.jp>
2024-04-09 11:45:38 +0900

    Commit remaining program for lesson5

--- Collection/src/sample/StreamSample.java
+++ Collection/src/sample/StreamSample.java
@@ -7,39 +7,38 @@ import java.util.stream.Collectors;
 public class StreamSample {
 
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
 		List<String> places = new ArrayList<>();
 
-        // 要素の追加
+        // add()
         places.add("アイスランド");
         places.add("スペイン");
         places.add("ナポリ");
         places.add("リスボン");
         places.add("ヨセミテ国立公園");
 
-        // forEachを使った単純な出力
+        // forEach
         System.out.println("■Stream forEachによる取得：");
         places.stream().forEach(place -> System.out.println(place));
 
-        // filterを使って特定の条件に一致する要素のみを抽出
+        // filter
         System.out.println("■「ン」で終わる場所のみ抽出：");
         places.stream()
               .filter(place -> place.endsWith("ン"))
               .forEach(place -> System.out.println(place));
 
-        // mapを使って各要素を加工
+        // map
         System.out.println("■各場所名の長さを表示：");
         places.stream()
               .map(place -> place + " の長さ: " + place.length())
               .forEach(place -> System.out.println(place));
 
-        // sortedを使ってソート
+        // sorted
         System.out.println("■ソート：");
         places.stream()
               .sorted()
               .forEach(place -> System.out.println(place));
 
-        // collectを使って結果を別のリストに格納
+        // collect
         List<String> filteredPlaces = places.stream()
                 .filter(place -> place.contains("ス"))
                 .collect(Collectors.toList());
